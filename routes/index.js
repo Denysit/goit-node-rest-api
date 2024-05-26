a047ad2015bcf10fdf65edc476ff84578663c0fc
Denysit <141247457+Denysit@users.noreply.github.com>
2024-05-26 03:07:39 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ import contactsRoutes from "./contacts.js";
 import userRoutes from "./auth.js";
 import authMiddleware from "../middleware/auth.js";
 
+
 const routes = express.Router();
 
 routes.use("/contacts", authMiddleware, contactsRoutes);
 routes.use("/users", userRoutes);
+
 
 export default routes;
